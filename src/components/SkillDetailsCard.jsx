e214c64de43618e32bd8d4506b3a7d44197675f4
KAKOLY-AKHTER <kakoly196@gmail.com>
2025-10-24 21:52:13 +0600

    update auth

--- src/components/SkillDetailsCard.jsx
+++ src/components/SkillDetailsCard.jsx
@@ -1,17 +1,17 @@
-// import React from "react";
-// import { Link } from "react-router";
+import React from "react";
+import { Link } from "react-router";
 
-// export default function SkillDetailsCard({ skill }) {
-//   if (!skill) return <div className="p-6">Skill not found</div>;
-//   return (
-//     <div className="space-y-5 md:py-30 py-60 ">
-//       <img className="w-full h-[350px] object-cover rounded" src={skill.image} alt={skill.skillName} />
-//       <h2 className="text-2xl font-semibold">{skill.skillName}</h2>
-//       <p>{skill.description}</p>
-//       <p className="text-sm text-gray-600">Provider: {skill.providerName} — {skill.providerEmail}</p>
-//       <Link className="btn btn-secondary" to="/">
-//         Back to Home
-//       </Link>
-//     </div>
-//   );
-// }
+export default function SkillDetailsCard({ skill }) {
+  if (!skill) return <div className="p-6">Skill not found</div>;
+  return (
+    <div className="space-y-5 md:py-30 py-60 ">
+      <img className="w-full h-[350px] object-cover rounded" src={skill.image} alt={skill.skillName} />
+      <h2 className="text-2xl font-semibold">{skill.skillName}</h2>
+      <p>{skill.description}</p>
+      <p className="text-sm text-gray-600">Provider: {skill.providerName} — {skill.providerEmail}</p>
+      <Link className="btn btn-secondary" to="/">
+        Back to Home
+      </Link>
+    </div>
+  );
+}
